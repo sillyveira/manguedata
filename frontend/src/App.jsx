a9503dce5a43c049af42ebd13dca01445350b533
sillyveira <wslc@cin.ufpe.br>
2025-03-12 16:16:55 -0300

    chore: Arquivos iniciais

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+    
+      <p className="text-2xl">
+        TailwindCSS instalado
+      </p>
+    </>
+  )
+}
+
+export default App
